8db8bf959c6dd7c885235f9b212e95ba09cea224
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-10 15:20:09 -0300

    feat: create route and config login user

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_login import LoginManager, login_user, current_user
 from models.user import User
 from database import db
 
@@ -6,7 +7,34 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+longin_maneger = LoginManager()
 db.init_app(app)
+longin_maneger.init_app(app)
+# view login
+longin_maneger.login_view = "login"
+
+
+@longin_maneger.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        # login
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticação realizada com sucesso"}), 200
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 
 @app.route("/hello-world", methods=["GET"])
