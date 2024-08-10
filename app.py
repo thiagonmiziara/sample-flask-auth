3af9b0befaf578770e87a8bae85f0ec9ac9861e5
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-10 19:21:46 -0300

    feat; config mysql database

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ from database import db
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud"
+)
 
 longin_maneger = LoginManager()
 db.init_app(app)
